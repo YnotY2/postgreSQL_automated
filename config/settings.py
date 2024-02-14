03c2a5e80ea121ff048ea7cc1d5ca0b61304e778
YnotY2 <131256977+YnotY2@users.noreply.github.com>
2024-02-14 22:00:28 +0000

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,8 @@
 import os
 
 # Database PostgreSQL Credentials
-postgresql_db_name = os.getenv("postgresql_db_name","yahoo_mail_sender")
-postgresql_db_passwd = os.getenv("postgresql_db_passwd", "G6OhE/mnoW_D!Ss/ocN")
-postgresql_db_usr = os.getenv("postgresql_db_usr", "yahoo_mail_sender")
+postgresql_db_name = os.getenv("postgresql_db_name","example_database")
+postgresql_db_passwd = os.getenv("postgresql_db_passwd", "example_password")
+postgresql_db_usr = os.getenv("postgresql_db_usr", "example_user")
 postgresql_port = os.getenv("postgresql_port", "5432")
 postgresql_host = os.getenv("postgresql_host", "localhost")
